29b3efef3d590f40c3135f24d2f7c1d7ef62b406
Rohit <rkumar8092378845@gmail.com>
2023-08-09 23:21:08 +0530

    Initial commit

---
+++ src/main/java/com/rkumar0206/mymuserauthenticationservice/MymUserAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rkumar0206.mymuserauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymUserAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymUserAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkumar0206/mymuserauthenticationservice/MymUserAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkumar0206.mymuserauthenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymUserAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
