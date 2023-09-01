b29e6742b68769924ddd9f3b12ca715afee0d031
Rohit <rkumar8092378845@gmail.com>
2023-09-01 17:02:19 +0530

    fixed build issue #1

--- src/test/java/com/rkumar0206/mymuserauthenticationservice/MymUserAuthenticationServiceApplicationTests.java
+++ src/test/java/com/rkumar0206/mymuserauthenticationservice/MymUserAuthenticationServiceApplicationTests.java
@@ -1,9 +1,8 @@
 package com.rkumar0206.mymuserauthenticationservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MymUserAuthenticationServiceApplicationTests {
 
 	@Test
